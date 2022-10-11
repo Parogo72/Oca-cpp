c5f78dfb788c8a34962b9f96a4574f3026c83a81
Parogo_72 <76404961+Parogo72@users.noreply.github.com>
2022-10-11 15:52:52 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -14,6 +14,8 @@ const int CASILLA_POZO = 31;
 const int CASILLA_LABERINTO = 42;
 const int CASILLA_MUERTE = 58;
 const int TURNOS_POSADA = 1;
+const int TURNOS_PRISION = 1;
+const int TURNOS_PUENTE = 3;
 const int DADO_MAXIMO = 6;
 const int DADO_MINIMO = 1;
 
@@ -38,7 +40,6 @@ int quienEmpieza();
 int efectoPosicion(int casillaActual);
 int efectoTiradas(int casillaActual, int numeroDeTiradas);
 int main() {
-	srand(time(NULL));
 	
 	return 0;
 }
@@ -48,35 +49,35 @@ bool esOca(int casilla) {
 }
 
 bool esPuente(int casilla) {
-	return casilla == CASILLA_PUENTE_1 || casilla == CASILLA_PUENTE_2;
+	
 }
 
 bool esDados(int casilla) {
-	return casilla == CASILLA_DADOS_1 || casilla == CASILLA_DADOS_2;
+	
 }
 
 bool esLaberinto(int casilla) {
-	return casilla == CASILLA_LABERINTO;
+	
 }
 
 bool esMuerte(int casilla) {
-	return casilla == CASILLA_MUERTE;
+	
 }
 
 bool esPosada(int casilla) {
-	return casilla == CASILLA_POSADA;
+	
 }
 
 bool esPrision(int casilla) {
-	return casilla == CASILLA_PRISION;
+	
 }
 
 bool esPozo(int casilla) {
-	return casilla == CASILLA_POZO;
+	
 }
 
 bool esMeta(int casilla) {
-	return casilla >= NUM_CASILLAS;
+	
 }
 
 int siguienteOca(int casilla) {
@@ -84,25 +85,25 @@ int siguienteOca(int casilla) {
 }
 
 int siguientePuente(int casilla) {
-	return casilla == CASILLA_PUENTE_1 ? CASILLA_PUENTE_1 : CASILLA_PUENTE_2;
+	
 }
 
 int siguienteDado(int casilla) {
-	return casilla == CASILLA_DADOS_1 ? CASILLA_DADOS_1 : CASILLA_DADOS_2;
+	
 }
 
 int siguienteLaberinto(int casilla) {
-	return casilla - 12;
+	
 }
 
 int siguienteMuerte(int casilla) {
-	return 1;
+	
 }
 
 int tirarDado() {
-	return DADO_MINIMO + rand() % (DADO_MAXIMO + 1 - DADO_MINIMO);
+	
 }
 
 int quienEmpieza() {
-	return 1 + rand() % (2 + 1 - 1);
+	
 }
