50947a8d92c3f73bcbb5b785136850d5466c6cd1
Parogo_72 <76404961+Parogo72@users.noreply.github.com>
2022-10-11 16:20:36 +0200

    missing variables

--- Source.cpp
+++ Source.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 const int NUM_CASILLAS = 63;
+const int CASILLA_PARTIDA = 1;
 const int CASILLA_PUENTE_1 = 6;
 const int CASILLA_PUENTE_2 = 12;
 const int CASILLA_DADOS_1 = 26;
@@ -16,8 +17,11 @@ const int CASILLA_MUERTE = 58;
 const int TURNOS_POSADA = 1;
 const int TURNOS_PRISION = 1;
 const int TURNOS_PUENTE = 3;
+const int PENALIZACION_LABERINTO = 12;
 const int DADO_MAXIMO = 6;
 const int DADO_MINIMO = 1;
+const int NUMERO_JUGADORES_MIN = 1;
+const int NUMERO_JUGADORES_MAX = 2;
 
 bool esOca(int casilla);
 bool esPuente(int casilla);
@@ -39,6 +43,7 @@ int tirarDado();
 int quienEmpieza();
 int efectoPosicion(int casillaActual);
 int efectoTiradas(int casillaActual, int numeroDeTiradas);
+
 int main() {
 	
 	return 0;
